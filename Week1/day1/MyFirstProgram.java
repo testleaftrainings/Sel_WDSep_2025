72d890e5227c888d43163e6fe041663d56a83051
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-09-03 13:32:40 +0530

    Add files via upload

---
+++ Week1/day1/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class MyFirstProgram {
+	// ctrl + space keys --> content assist 
+	public static void main(String[] args) {
+		System.out.println("Hello Java World");
+		System.out.println("Testleaf");
+		
+		   
+	}
+
+}
